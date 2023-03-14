c26278e63b9aa35cc3152c4d48b34954e1e89266
Michael Curry <Michael.Curry@Volution-Group.com>
2023-03-14 12:04:16 +0000

    Change the scope of _client and _address so that they can be set / updated directly externally (not always known when class instantiated)

--- ModbusClient/ClientSlave.cs
+++ ModbusClient/ClientSlave.cs
@@ -8,82 +8,76 @@ namespace VVG.Modbus
 {
     public class ClientSlave
     {
-        ClientRTU _client;
-        byte _address;
-
-        public ClientSlave(ClientRTU client, byte slaveID)
-        {
-            _client = client;
-            _address = slaveID;
-        }
-
+        public ClientRTU Client { get; set; }
+        public byte Address { get; set; }
+        
         public bool ReadCoil(UInt16 coilNo)
         {
-            return _client.ReadCoil(_address, coilNo);
+            return Client.ReadCoil(Address, coilNo);
         }
 
         public bool[] ReadCoils(UInt16 coilStartNo, UInt16 len)
         {
-            return _client.ReadCoils(_address, coilStartNo, len);
+            return Client.ReadCoils(Address, coilStartNo, len);
         }
 
         public void WriteCoil(UInt16 coilNo, bool txCoil)
         {
-            _client.WriteCoil(_address, coilNo, txCoil);
+            Client.WriteCoil(Address, coilNo, txCoil);
         }
 
         public void WriteCoils(UInt16 coilStartNo, bool[] txCoils)
         {
-            _client.WriteCoils(_address, coilStartNo, txCoils);
+            Client.WriteCoils(Address, coilStartNo, txCoils);
         }
 
         public bool ReadDiscreteInput(UInt16 inputNo)
         {
-            return _client.ReadDiscreteInput(_address, inputNo);
+            return Client.ReadDiscreteInput(Address, inputNo);
         }
 
         public bool[] ReadDiscreteInputs(UInt16 inputStartNo, UInt16 len)
         {
-            return _client.ReadDiscreteInputs(_address, inputStartNo, len);
+            return Client.ReadDiscreteInputs(Address, inputStartNo, len);
         }
 
         public UInt16 ReadHoldingReg(UInt16 regNo)
         {
-            return _client.ReadHoldingReg(_address, regNo);
+            return Client.ReadHoldingReg(Address, regNo);
         }
 
         public UInt16[] ReadHoldingRegs(UInt16 regStartNo, UInt16 len)
         {
-            return _client.ReadHoldingRegs(_address, regStartNo, len);
+            return Client.ReadHoldingRegs(Address, regStartNo, len);
         }
 
         public void WriteHoldingReg(UInt16 regNo, UInt16 txReg)
         {
-            _client.WriteHoldingReg(_address, regNo, txReg);
+            Client.WriteHoldingReg(Address, regNo, txReg);
         }
 
         public void WriteHoldingRegs(UInt16 startRegNo, UInt16[] txRegs)
         {
-            _client.WriteHoldingRegs(_address, startRegNo, txRegs);
+            Client.WriteHoldingRegs(Address, startRegNo, txRegs);
         }
 
         public UInt16 ReadInputReg(UInt16 regNo)
         {
-            return _client.ReadInputReg(_address, regNo);
+            return Client.ReadInputReg(Address, regNo);
         }
 
         public UInt16[] ReadInputRegs(UInt16 regStartNo, UInt16 len)
         {
-            return _client.ReadInputRegs(_address, regStartNo, len);
+            return Client.ReadInputRegs(Address, regStartNo, len);
         }
 
         public byte[] ReadFileRecord(UInt16 fileNo, UInt16 recNo, UInt16 len)
         {
-            return _client.ReadFileRecord(_address, fileNo, recNo, len);
+            return Client.ReadFileRecord(Address, fileNo, recNo, len);
         }
         public void WriteFileRecord(UInt16 fileNo, UInt16 recNo, byte[] fileRecs)
         {
-            _client.WriteFileRecord(_address, fileNo, recNo, fileRecs);
+            Client.WriteFileRecord(Address, fileNo, recNo, fileRecs);
         }
     }
 }
