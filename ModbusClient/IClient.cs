cc05116a066b5c0adfbda60e9e326adfe8c71cf0
Michael Curry <Michael.Curry@Volution-Group.com>
2023-03-17 14:50:00 +0000

    Updated comment - max register is 9998

--- ModbusClient/IClient.cs
+++ ModbusClient/IClient.cs
@@ -12,7 +12,7 @@ namespace VVG.Modbus
         /// Read a single coil register
         /// </summary>
         /// <param name="addr">Address of the modbus slave</param>
-        /// <param name="coilNo">Coil register number (0..9999)</param>
+        /// <param name="coilNo">Coil register number (register 0000..9998 = address 0001..9999)</param>
         /// <returns>Coil value read</returns>
         Task<bool> ReadCoil(byte addr, UInt16 coilNo);
 
@@ -20,7 +20,7 @@ namespace VVG.Modbus
         /// Read multiple coil registers
         /// </summary>
         /// <param name="addr">Address of the modbus slave</param>
-        /// <param name="coilStartNo">First coil register number to read (0..9999)</param>
+        /// <param name="coilStartNo">First coil register number to read (register 0000..9998 = address 0001..9999)</param>
         /// <param name="len">Number of coils to read</param>
         /// <returns>Coil values read</returns>
         Task<IEnumerable<bool>> ReadCoils(byte addr, UInt16 coilStartNo, UInt16 len);
@@ -29,7 +29,7 @@ namespace VVG.Modbus
         /// Read single discrete input register
         /// </summary>
         /// <param name="addr">Address of the modbus slave</param>
-        /// <param name="inputNo">Input register number (0..9999)</param>
+        /// <param name="inputNo">Input register number (register 0000..9998 = address 0001..9999)</param>
         /// <returns>Input register value read</returns>
         Task<bool> ReadDiscreteInput(byte addr, UInt16 inputNo);
 
@@ -37,7 +37,7 @@ namespace VVG.Modbus
         /// Read multiple discrete input registers
         /// </summary>
         /// <param name="addr">Address of the modbus slave</param>
-        /// <param name="inputStartNo">First input register number to read (0..9999)</param>
+        /// <param name="inputStartNo">First input register number to read (register 0000..9998 = address 0001..9999)</param>
         /// <param name="len">Number of input registers to read</param>
         /// <returns>Input register values read</returns>
         Task<IEnumerable<bool>> ReadDiscreteInputs(byte addr, UInt16 inputStartNo, UInt16 len);
@@ -46,7 +46,7 @@ namespace VVG.Modbus
         /// Read single holding register
         /// </summary>
         /// <param name="addr">Address of the modbus slave</param>
-        /// <param name="regNo">Holding register number (0..9999)</param>
+        /// <param name="regNo">Holding register number (register 0000..9998 = address 0001..9999)</param>
         /// <returns>Holding register value read</returns>
         Task<UInt16> ReadHoldingRegister(byte addr, UInt16 regNo);
 
@@ -54,7 +54,7 @@ namespace VVG.Modbus
         /// Read multiple holding registers
         /// </summary>
         /// <param name="addr">Address of the modbus slave</param>
-        /// <param name="regStartNo">First holding register number to read (0..9999)</param>
+        /// <param name="regStartNo">First holding register number to read (register 0000..9998 = address 0001..9999)</param>
         /// <param name="len">Number of holding registers to read</param>
         /// <returns>Holding register values read</returns>
         Task<IEnumerable<UInt16>> ReadHoldingRegisters(byte addr, UInt16 regStartNo, UInt16 len);
@@ -63,7 +63,7 @@ namespace VVG.Modbus
         /// Read single input register
         /// </summary>
         /// <param name="addr">Address of the modbus slave</param>
-        /// <param name="regNo">Input register number (0..9999)</param>
+        /// <param name="regNo">Input register number (register 0000..9998 = address 0001..9999)</param>
         /// <returns>Input register value read</returns>
         Task<UInt16> ReadInputRegister(byte addr, UInt16 regNo);
 
@@ -71,7 +71,7 @@ namespace VVG.Modbus
         /// Read multiple input registers
         /// </summary>
         /// <param name="addr">Address of the modbus slave</param>
-        /// <param name="regStartNo">First input register number to read (0..9999)</param>
+        /// <param name="regStartNo">First input register number to read (register 0000..9998 = address 0001..9999)</param>
         /// <param name="len">Number of input registers to read</param>
         /// <returns>Input register values read</returns>
         Task<IEnumerable<UInt16>> ReadInputRegisters(byte addr, UInt16 regStartNo, UInt16 len);
@@ -80,7 +80,7 @@ namespace VVG.Modbus
         /// Write to a single coil
         /// </summary>
         /// <param name="addr">Address of the modbus slave</param>
-        /// <param name="coilNo">Coil register number (0..9999)</param>
+        /// <param name="coilNo">Coil register number (register 0000..9998 = address 0001..9999)</param>
         /// <param name="txCoil">Value to set</param>
         Task WriteCoil(byte addr, UInt16 coilNo, bool txCoil);
 
@@ -88,7 +88,7 @@ namespace VVG.Modbus
         /// Write to a single holding register
         /// </summary>
         /// <param name="addr">Address of the modbus slave</param>
-        /// <param name="regNo">Holding register number (0..9999)</param>
+        /// <param name="regNo">Holding register number (register 0000..9998 = address 0001..9999)</param>
         /// <param name="txReg">Value to set</param>
         Task WriteHoldingRegister(byte addr, UInt16 regNo, UInt16 txReg);
 
@@ -96,7 +96,7 @@ namespace VVG.Modbus
         /// Write to multiple coils
         /// </summary>
         /// <param name="addr">Address of the modbus slave</param>
-        /// <param name="coilStartNo">First coil register number to write to (0..9999)</param>
+        /// <param name="coilStartNo">First coil register number to write to (register 0000..9998 = address 0001..9999)</param>
         /// <param name="txCoils">Values to set</param>
         Task WriteCoils(byte addr, UInt16 coilStartNo, bool[] txCoils);
 
@@ -104,7 +104,7 @@ namespace VVG.Modbus
         /// Write to multiple holding registers
         /// </summary>
         /// <param name="addr">Address of the modbus slave</param>
-        /// <param name="regStartNo">First holding register number to write to (0..9999)</param>
+        /// <param name="regStartNo">First holding register number to write to (register 0000..9998 = address 0001..9999)</param>
         /// <param name="txRegs">Values to write</param>
         Task WriteHoldingRegisters(byte addr, UInt16 regStartNo, UInt16[] txRegs);
 
