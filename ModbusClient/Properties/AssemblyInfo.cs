da61be82139bcfbdca541fe99697eca24c2f5026
mcurry <Michael.Curry@Volution-Group.com>
2024-03-22 11:19:41 +0000

    Updated to latest log4net; added NuPack to generate nupkg output for VVG.Modbus

--- ModbusClient/Properties/AssemblyInfo.cs
+++ ModbusClient/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModbusClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("VVG.ModbusClient")]
+[assembly: AssemblyDescription("Modbus RTU client with access to coils, discrete inputs, holding registers, input registers and file records")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ModbusClient")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCompany("Volution Ventilation Group")]
+[assembly: AssemblyProduct("VVG.ModbusClient")]
+[assembly: AssemblyCopyright("Copyright © Volution Ventilation Group 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
